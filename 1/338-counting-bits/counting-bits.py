49cdc1b1ea93e7d513f2130000b8b505e422e4dd
Utkarsh Sharma <utkarshstudent1@gmail.com>
2024-06-12 01:22:50 +0530

    Time: 43 ms (88.72%) | Memory: 15.7 MB (10.30%) - LeetSync

--- 1/338-counting-bits/counting-bits.py
+++ 1/338-counting-bits/counting-bits.py
@@ -6,10 +6,11 @@ class Solution(object):
         """
         ans=[0]
         for i in range(1, n+1):
+            div = i//2
             if(i%2==1):
-                ans.append(ans[i//2]+1)
+                ans.append(ans[div]+1)
             else:
-                ans.append(ans[i//2])
+                ans.append(ans[div])
 
         return ans
 
