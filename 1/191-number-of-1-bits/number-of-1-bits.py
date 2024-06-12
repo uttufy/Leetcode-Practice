2f5462ae13d2093281041d2f688c8716f4d4da72
Utkarsh Sharma <utkarshstudent1@gmail.com>
2024-06-12 22:06:10 +0530

    Time: 15 ms (57.43%) | Memory: 11.7 MB (33.78%) - LeetSync

---
+++ 1/191-number-of-1-bits/number-of-1-bits.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        def convert_dec_to_bin(self, n):
+            bin = ""
+            while n>0:
+                rem = n % 2
+                bin = bin + str(rem)
+                n //= 2
+
+            return bin[::-1]
+        
+
+        res = convert_dec_to_bin(self,n)
+        count = 0
+
+        for c in res:
+            if c == '1': count+=1
+
+        return count
+
+
+        
