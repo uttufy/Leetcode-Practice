f76ab760e5b0351528d204f0c4d7dd35c7e4b899
Utkarsh Sharma <utkarshstudent1@gmail.com>
2024-06-12 22:26:34 +0530

    Time: 15 ms (57.43%) | Memory: 11.5 MB (67.87%) - LeetSync

--- 1/191-number-of-1-bits/number-of-1-bits.py
+++ 1/191-number-of-1-bits/number-of-1-bits.py
@@ -4,24 +4,37 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+
+        ## More optimised
+
+        count = 0 
+        while n>0:
+            count += n%2
+            n //=2
         
-        def convert_dec_to_bin(self, n):
-            bin = ""
-            while n>0:
-                rem = n % 2
-                bin = bin + str(rem)
-                n //= 2
-
-            return bin[::-1]
-        
-
-        res = convert_dec_to_bin(self,n)
-        count = 0
-
-        for c in res:
-            if c == '1': count+=1
-
         return count
+
+
+
+        ## Brute force 
+        
+        # def convert_dec_to_bin(self, n):
+        #     bin = ""
+        #     while n>0:
+        #         rem = n % 2
+        #         bin = bin + str(rem)
+        #         n //= 2
+
+        #     return bin[::-1]
+        
+
+        # res = convert_dec_to_bin(self,n)
+        # count = 0
+
+        # for c in res:
+        #     if c == '1': count+=1
+
+        # return count
 
 
         
